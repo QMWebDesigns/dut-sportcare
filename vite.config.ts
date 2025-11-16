db56f77d9263e46c7af75c091fb0f8db3a6051ce
QM Web Designs <qmwebdesigns24@gmail.com>
2025-11-17 01:33:24 +0200

    Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/', 
+  base: './', // Use relative paths
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
